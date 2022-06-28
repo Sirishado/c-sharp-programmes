91f61dd5f5198a80ae1b01ddc7d1a5e5697be887
parvathi D <parvathid@zirafftechnologies.com>
2022-06-28 18:47:35 +0530

    Add project files.

---
+++ Practise Programmes/Bank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_Programmes
+{
+    public class Bank
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string BankName { get; set; }
+        public string BranchName { get; set; }
+        public string AccountNumber { get; set; }
+    }
+
+    public class Months
+    {
+        public List<string> MonthNames { get; set; }
+        public List<string> WeekNames { get; set; }
+    }
+}
---
+++ Practise Programmes/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practise_Programmes
+{
+    class Program
+    {
+        
+        public void Getstring()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public async Task GetData()
+        {
+            try
+            {
+                string Sentence = "This Is Practise Program";
+                Console.WriteLine(Sentence.Contains("Practise"));
+                Console.WriteLine(Sentence.Contains("example"));
+            }
+            catch(Exception ex)
+            {
+                throw ;
+            }
+        }
+        public  async Task<Bank> GetDetails()
+        {
+            try
+            {
+                Bank bank = new();
+                bank.Id = Guid.NewGuid();
+                bank.BankName = "ICICI";
+                bank.BranchName = "Hyderabad";
+                bank.Name = "Sirisha";
+                bank.AccountNumber = "123456";
+                List<Bank> lstbanks = new();
+                lstbanks.Add(bank);
+                var data =  (from banks in lstbanks.AsQueryable()
+                                  where banks.Name == "Sirisha"
+                                  select new Bank()
+                                  {
+                                      Id = banks.Id,
+                                      BranchName = banks.BranchName,
+                                      BankName = banks.BankName,
+                                      Name = banks.Name,
+                                      AccountNumber = banks.AccountNumber
+
+
+                                  }).ToList();
+                var bankDetails = lstbanks.Where(k => k.BranchName == "Hyderabad").Select(i => i).Skip(0).Take(1).FirstOrDefault();
+                return  bank;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public Months Details()
+        {
+            try
+            {
+
+                Months months = new();
+                months.MonthNames = "Jan,Feb,March,April".Split(',').ToList();
+                months.WeekNames = "sunday,Monday,Friday,Saturday".Split(',').ToList();
+                return months;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
